d90c565a6e07ad357846448f771c154ed8b72550
JRoccaMonti <Rocca.Jano@gmail.com>
2023-10-10 23:09:00 -0300

    erickkkkkkkkk

--- src/models/professional.js
+++ src/models/professional.js
@@ -1,127 +1,102 @@
-const { conn } = require('../DB_connection');
 const { DataTypes } = require('sequelize');
 
-const Data = {
-  name: DataTypes.STRING,
-  lastname: DataTypes.STRING,
-  age: DataTypes.INTEGER,
-  dni: DataTypes.INTEGER,
-};
-const DataEdu = {
-  institution: DataTypes.STRING,
-  year_end: DataTypes.DATE,
-  degree: DataTypes.STRING,
-};
-
-const DataEx = {
-  date_start: DataTypes.DATE,
-  date_end: DataTypes.DATE,
-  company: DataTypes.STRING,
-  description: DataTypes.STRING,
-};
-
-
-const Professional = conn.define('Professional', {
-
-  id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    unique: true,
-  },
-
-  data: {
-    type: DataTypes.JSONB,
-    allowNull: false,
-    defaultValue: Data,
-    validate: {
-      isValidJSON(value) {
-        if (!value.name || !value.email) {
-          throw new Error('error validating.');
-        }
-      },
+module.exports = (sequelize) => {
+  const Professional = sequelize.define('Professional', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      unique: true,
     },
-  },
-
-  experience: {
-    type: DataTypes.JSONB,
-    allowNull: false,
-    defaultValue: [],
-    validate: {
-      isValidExperienceArray(value) {
-        if (!Array.isArray(value)) {
-          throw new Error('experience debe ser un array de objetos.');
-        }
-        for (const obj of value) {
-          if (
-            typeof obj.date_start !== 'object' ||
-            typeof obj.date_end !== 'object' ||
-            typeof obj.company !== 'string' ||
-            typeof obj.description !== 'string'
-          ) {
-            throw new Error('Cada objeto en experience debe tener la estructura correcta.');
+    data: {
+      type: DataTypes.JSONB,
+      allowNull: false,
+      defaultValue: {
+        name: DataTypes.STRING,
+        lastname: DataTypes.STRING,
+        age: DataTypes.INTEGER,
+        dni: DataTypes.INTEGER,
+      },
+      validate: {
+        isValidJSON(value) {
+          if (!value.name || !value.lastname || !value.age || !value.dni) {
+            throw new Error('Error de validación.');
           }
-        }
+        },
       },
     },
-  },
-
-  education: {
-    type: DataTypes.JSONB,
-    allowNull: false,
-    defaultValue: [],
-    validate: {
-      isValidExperienceArray(value) {
-        if (!Array.isArray(value)) {
-          throw new Error('experience debe ser un array de objetos.');
-        }
-        for (const obj of value) {
-          if (
-            typeof obj.year_end !== 'object' ||
-            typeof obj.degree !== 'string' ||
-            typeof obj.institution !== 'string'
-          ) {
-            throw new Error('Cada objeto en experience debe tener la estructura correcta.');
+    experience: {
+      type: DataTypes.JSONB,
+      allowNull: false,
+      defaultValue: [],
+      validate: {
+        isValidExperienceArray(value) {
+          if (!Array.isArray(value)) {
+            throw new Error('experience debe ser un array de objetos.');
           }
-        }
+          for (const obj of value) {
+            if (
+              typeof obj.date_start !== 'object' ||
+              typeof obj.date_end !== 'object' ||
+              typeof obj.company !== 'string' ||
+              typeof obj.description !== 'string'
+            ) {
+              throw new Error('Cada objeto en experience debe tener la estructura correcta.');
+            }
+          }
+        },
       },
     },
-  },
+    education: {
+      type: DataTypes.JSONB,
+      allowNull: false,
+      defaultValue: [],
+      validate: {
+        isValidExperienceArray(value) {
+          if (!Array.isArray(value)) {
+            throw new Error('experience debe ser un array de objetos.');
+          }
+          for (const obj of value) {
+            if (
+              typeof obj.year_end !== 'object' ||
+              typeof obj.degree !== 'string' ||
+              typeof obj.institution !== 'string'
+            ) {
+              throw new Error('Cada objeto en experience debe tener la estructura correcta.');
+            }
+          }
+        },
+      },
+    },
+    languages: {
+      type: DataTypes.ARRAY(DataTypes.INTEGER),
+      allowNull: false,
+    },
+    nationality: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    development_skills: {
+      type: DataTypes.ARRAY(DataTypes.INTEGER),
+      allowNull: false,
+    },
+    extra_information: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    briefcase: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    cci: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    state: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+    },
+  });
 
-  languages: {
-    type: DataTypes.ARRAY(DataTypes.INTEGER),
-    allowNull: false,
-  },
-
-  nationality: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-  },
-
-  development_skills: {
-    type: DataTypes.ARRAY(DataTypes.INTEGER),
-    allowNull: false,
-  },
-
-  extra_information: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  
-  briefcase: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-
-  cci: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-  },
-
-  state: {
-    type: DataTypes.BOOLEAN,
-    allowNull: false,
-  },
-
-});
-
-module.exports = Professional;
+  return Professional;
+};
--- src/models/user.js
+++ src/models/user.js
@@ -1,38 +1,40 @@
-const { conn } = require('../DB_connection');
+
 const { DataTypes } = require('sequelize');
 
-const User = conn.define('User', {
-    id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        unique: true,
-    },
-    username: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
-    },
-    email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
-        validate: {
-            isEmail: true,
+module.exports = (sequelize) => {
+    const User = sequelize.define('User', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            allowNull: false,
+            unique: true,
         },
-    },
-    password: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    type: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-    },
-    state: {
-        type: DataTypes.BOOLEAN,
-        allowNull: false,
-    },
-});
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true,
+            },
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        type: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        state: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+        },
+    });
 
-module.exports = User;
-
+    return User;
+};
