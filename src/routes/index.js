356988bf7f1d0030cb9e8b86e0b19e44f653bfd9
gunminiho <gunminiho@gmail.com>
2023-10-12 11:00:24 -0500

    rutas + server cors

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,3 +1,18 @@
 const { Router } = require('express')
+const router = Router();
 
 
+//GETS
+
+router.get("/company", () => { });
+router.get("/itskills", () => { });
+router.get("/language", () => { });
+router.get("/location", () => { });
+router.get("/professional", () => { });
+router.get("/projecttype", () => { });
+router.get("/review", () => { });
+router.get("/user", () => { });
+router.get("/nationality", () => { });
+
+
+module.exports = router;
