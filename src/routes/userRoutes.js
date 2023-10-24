85a6cd201581624cf45586c84f77152d4ab45cd2
jFranzBel <jonnathan.franz.87@outlook.com>
2023-10-23 18:53:57 -0600

    Se agrego la logica de auth0 ( middleware, controlador y rutas en userRoutes)

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,5 +1,9 @@
 const { Router } = require('express');
 const userController = require('../controllers/userController');
+//---------------   Auth0
+const userAuth0Middleware = require('../middlewares/userAuth0Middleware');
+const userAuth0Controller = require('../controllers/userAuth0Controller');
+//---------------
 const router = Router();
 
 router.get('/user/:id', userController.getUserById);
@@ -8,5 +12,7 @@ router.post('/user', userController.createUser);
 router.put('/user/:id', userController.updateUser);
 router.delete('/user/:id', userController.deleteUser);
 router.post('/user/login', userController.login);
-
+//---------------   Auth0
+router.post('/user/auth0/loginOrSignup', userAuth0Middleware, userAuth0Controller.loginOrSignup);
+//--------------- 
 module.exports = router;
