1d7bdee9b2ddad5bb3667b872458099a4bf6a2e9
gunminiho <gunminiho@gmail.com>
2023-10-24 12:51:24 -0500

    mercadopago integrado v2

--- src/controllers/paymentController.js
+++ src/controllers/paymentController.js
@@ -79,16 +79,22 @@ const listenWebhook = async (req, res) => {
 
 const getPaymentsById = async (req, res) => {
     const { from } = req.params;
-    console.log(from);
-    const payments = await Payment.findAll(
-        {
-            where: {
-                from,
-            }
+    if (from)
+        try {
+            const payments = await Payment.findAll(
+                {
+                    where: {
+                        from,
+                    }
+                }
+            );
+            if (payments)
+                return res.status(200).json(payments)
+            return res.status(404).send("No se encontraron pagos para este id");
+        } catch (error) {
+            return res.status(500).send(error.message);
         }
-    );
-    console.log(payments);
-    res.json(payments)
+    return res.status(500).send("No se encontro ningun id para buscar");
 };
 
 
