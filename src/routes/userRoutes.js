f2869c122dedb69023ab26bfc5a75212157642c1
JRoccaMonti <Rocca.Jano@gmail.com>
2023-10-27 20:57:01 -0300

    aut0 2

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,6 +2,7 @@ const { Router } = require('express');
 const userController = require('../controllers/userController');
 const userAuth0Middleware = require('../utils/auth0Middleware');
 const userAuth0Controller = require('../controllers/userAuth0Controller');
+const updateAuth0UserTypeController = require('../controllers/updateAuth0UserTypeController')
 const router = Router();
 
 router.get('/user/:id', userController.getUserById);
@@ -10,7 +11,10 @@ router.post('/user', userController.createUser);
 router.put('/user/:id', userController.updateUser);
 router.delete('/user/:id', userController.deleteUser);
 router.post('/user/login', userController.login);
+
 router.post('/user/auth0/loginOrSignup', userAuth0Middleware, userAuth0Controller.loginOrSignup);
+router.put('/user/auth0/register', updateAuth0UserTypeController.updateAuth0UserTypeController )
+
 router.post('/user/verifyemail', userController.verifyemail);
 
 module.exports = router;
