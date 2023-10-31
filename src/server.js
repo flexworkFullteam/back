a2e9402090b1066b416d4e147d952d3560508889
flexworkFullteam <147434500+flexworkFullteam@users.noreply.github.com>
2023-10-31 13:30:36 -0600

    Update server.js
    fixing db issues try7...

--- src/server.js
+++ src/server.js
@@ -11,51 +11,32 @@ const morgan = require('morgan');
 const { conn } = require('./DB_connection');
 const helmet = require('helmet'); 
 
-// Create an Express application
+
 const server = express();
 
-// Logging middleware for incoming requests
 server.use(morgan('dev'));
-
-
-// Security middleware to set various HTTP headers
 server.use(helmet());
-
-// CORS configuration for allowing requests only from specified domains
-server.use(cors({
-    origin: FRONT_URL,
-    credentials: true,
-}));
-
-// Built-in middleware function to parse incoming requests with URL encoded payloads
+server.use(cors());
 server.use(express.urlencoded({ extended: true, limit: '50mb' }));
-
-// Built-in middleware function to parse incoming requests with JSON payloads
 server.use(express.json({ limit: '50mb' }));
-
-// Middleware to parse cookies from the request headers
 server.use(cookieParser());
 
-// Middleware to handle CORS pre-flight requests
-
 server.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', FRONT_URL);
+    res.header('Access-Control-Allow-Origin', "https://front-virid-sigma.vercel.app/");
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
     next();
 });
 
-// Middleware to handle application routes
 server.use('/', routes);
 
-
-// Centralized error handling middleware
 server.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Something broke!');
 });
 
+//-------- db connection --------
 const maxAttempts = 60;
 let attempts = 0;
 
@@ -84,7 +65,6 @@ const retryConnection = () => {
 
 server.use(async (req, res, next) => {
     try {
-        // Example query to check the connection
         await conn.query('SELECT 1+1 AS result');
         next();
     } catch (error) {
