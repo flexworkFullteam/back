e17bfce1b6f413db763e9e73998eca2180ffc830
flexworkFullteam <147434500+flexworkFullteam@users.noreply.github.com>
2023-10-31 13:16:33 -0600

    Update DB_connection.js
    Fixing db issues try5..

--- src/DB_connection.js
+++ src/DB_connection.js
@@ -3,13 +3,12 @@ const { Sequelize } = require("sequelize");
 
 const fs = require('fs');
 const path = require('path');
-const {
-  DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME
-} = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`, {
-  logging: false,
-  native: false,
+// Create a new instance of Sequelize with database connection details
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/flexworks_722h`, {
+  logging: false, 
+  native: false, 
 });
 
 //carga automatica de modelos
