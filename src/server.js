2e84f2971c19960d82e4890b491d1c1a94cd591d
flexworkFullteam <147434500+flexworkFullteam@users.noreply.github.com>
2023-10-31 21:38:53 -0600

    Update server.js
    Removing { force: true } from conn.sync.

--- src/server.js
+++ src/server.js
@@ -43,7 +43,7 @@ let attempts = 0;
 const tryConnect = async () => {
     try {
         await conn.authenticate();
-        await conn.sync({ force: true });
+        await conn.sync();
         console.log('Database connected successfully.');
         server.listen(3001, () => {
             console.log('Server listening at port 3001');
